ce12d9437b5ed2e5b5cd4dcbb47265df3ca0f1cc
alessandrogammieri <alessandrogammieri.it@gmail.com>
2019-09-20 09:23:02 +0200

    update elements

---
+++ api-edit.php
@@ -0,0 +1,38 @@
+<?php 
+
+    header('Content-type: application/json');
+
+    $servername = 'localhost';
+    $username = 'root';
+    $password = 'root';
+    $dbname = 'bar_db';
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn -> connect_error) {
+    var_dump('error');
+    var_dump($conn);
+    die();
+    }
+
+    $drink_name = $_GET['drink_name'];
+    $brand = $_GET['brand'];
+    $price = $_GET['price'];
+    $expiration_date = $_GET['expiration_date'];
+    $id = $_GET['id'];
+
+    $query = "
+        UPDATE bancone
+        SET drink_name = '" . $drink_name . "',
+            brand = '" . $brand . "',
+            price = '" . $price . "',
+            expiration_date = '" . $expiration_date . "'
+        WHERE id = " . $id . "
+    ";
+
+    $res = $conn -> query($query);
+
+    $conn->close();
+    echo json_encode($res);
+
+?>
--- script.js
+++ script.js
@@ -71,10 +71,41 @@ function addDrink() {
     $(".date").val("");
 }
 
+function updateDrink() {  
+    var element = $(this);
+    var box = element.parent();
+    var idDrink = box.data("id") 
+    var drink_name = prompt("Modifica il nome del Drink");
+    var brand = prompt("Modifica la marca del Drink");
+    var price = prompt("Modifica il prezzo del Drink");
+    var date = prompt("Modifica la data di scadenza");
+    var mom = moment(date, "YYYY-MM-DD");
+    var expiration_date = mom.format("YYYY-MM-DD");
+    $.ajax({
+        url: "api-edit.php",
+        method: "GET",
+        data: { 
+            id: idDrink,
+            drink_name: drink_name,
+            brand: brand,
+            price: price,
+            expiration_date: expiration_date
+        },
+        success: function () {
+            console.log("Drink: " + drink_name + " " + "Marca: " + brand + " " + "Prezzo: " + price + " " + "Scadenza: " + expiration_date + " " + "ID: " + idDrink);
+            getDrink();
+        },
+        error: function () {
+            alert("C'è stato un errore nella modifica");
+        }
+    })
+}
+
 function init() {
     getDrink();
     $(document).on("click", "#delete", deleteDrink);
     $(document).on("click", "#add", addDrink);
+    $(document).on("click", "#update", updateDrink);
 }
 
 $(document).ready(init);
